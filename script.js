208f759194573177f9e73f56a193fc09fb12dc2f
Marin Luic <46132985+AcidBurnHen@users.noreply.github.com>
2019-01-05 20:15:51 +0100

    added lodash

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const _ = require("lodash");
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
+
 	body.style.background =
     "Linear-gradient(to right, "
     + color1.value
